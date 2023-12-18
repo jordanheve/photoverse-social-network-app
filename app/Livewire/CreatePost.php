61d26b598d091ce43dedc952b4214effaa6dc883
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2023-12-18 16:12:45 -0600

    Implemented image upload and storage for posts
    This commit introduces functionality to upload images for new posts. Changes include:
    - Added Livewire component for handling image uploads
    - Implemented Spatie Media Library for managing uploaded images
    - Modified Post model to store unique image names in the database
    - Added validation to ensure images do not exceed 10MB
    - Updated the Post creation process to include uploaded images

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
+use Illuminate\Support\Str;
 use Livewire\WithFileUploads;
 
 class CreatePost extends Component
@@ -10,11 +12,38 @@ class CreatePost extends Component
     use WithFileUploads;
     public $title; 
     public $description;
- 
     public $image;
+
+    public $rules = [
+        'image' => 'required|image|max:10000',
+        'title' => 'required| max:255',
+        'description' => 'required|max255'
+    ];
+
     public function store()
     {
-        
+        $uniqueFileName = Str::uuid()->toString() . '.' . $this->image->getClientOriginalExtension();
+        dd($uniqueFileName);
+        $this->validate();
+        // File is an image that is < 10mb
+
+        // Validation Here
+ 
+       
+        $post = Post::create([
+            'title'=> $this->title,
+            'description' => $this->description,
+            'image'=> $uniqueFileName,
+
+        ]);
+ 
+        // Add the file to the collection
+        $post->addMedia($this->image->getRealPath())
+        ->usingFileName($uniqueFileName)
+        ->toMediaCollection('image');
+ 
+        // Redirect or send back success message
+
     }
 
     public function render()
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,9 +1,11 @@
-<form wire:submit.prevent="create" novalidate class="flex items-center gap-10 m-4">
+<form wire:submit.prevent="store" novalidate class="flex items-center gap-10 m-4">
     <div class="w-96">
 
         
         <x-filepond wire:model='image'/>
-    
+        @error('image')
+        <x-alert-message> {{$message}}</x-alert-message>
+        @enderror
 </div>
 
     <div>
@@ -19,9 +21,11 @@
                 ariaLabel="Title"
                 required
                 />
+                @error('title')
                 <x-alert-message >
-                    
+                    {{$message}}
                 </x-alert-message>
+                @enderror
             </div>
         
                 
@@ -30,6 +34,12 @@
                 class="resize-none w-full text-xs outline-none p-2 rounded-sm border-zinc-300 border bg-zinc-100" 
                 placeholder="Description" id="description"  rows="3"></textarea>
                 
+                @error('description')
+                <x-alert-message >
+                    {{$message}}
+                </x-alert-message>
+                @enderror
+
             <x-register-button type='submit'>Post</x-register-button>
         
         </div>
