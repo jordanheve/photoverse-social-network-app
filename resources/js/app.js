1857d56a111d5e30f88c566c155418531f3e557c
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2023-12-27 13:00:13 -0600

    Integrate SweetAlert2 for handling alerts on password reset page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 import * as FilePond from 'filepond';
 import 'filepond/dist/filepond.min.css';
+import Swal from 'sweetalert2';
  // Import the plugin code
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 
@@ -8,4 +9,6 @@ import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 FilePond.registerPlugin(FilePondPluginImagePreview);
 window.FilePond = FilePond;
+window.Swal = Swal;
+
 
